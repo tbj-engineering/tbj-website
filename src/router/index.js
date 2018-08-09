b7b18de5621ffee9f16727eee82096b4189dd0c3
Benjamin Eason <benjamin.eason@me.com>
2018-08-09 21:22:54 +0100

    Addition of a route

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Products from '@/components/Products';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld,
+    },
+    {
+      path: '/products',
+      name: 'Products',
+      component: Products,
+      meta: {
+        title: 'Products',
+      },
     },
   ],
 });
