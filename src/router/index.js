37f553def6e5eb54ed782118b5b1ab48ae2f9f19
Benjamin Eason <benjamin.eason@me.com>
2018-08-09 21:34:55 +0100

    Build 7

--- src/router/index.js
+++ src/router/index.js
@@ -6,20 +6,12 @@ import Products from '@/components/Products';
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld,
-    },
-    {
-      path: '/products',
-      name: 'Products',
-      component: Products,
-      meta: {
-        title: 'Products',
-      },
     },
   ],
 });
