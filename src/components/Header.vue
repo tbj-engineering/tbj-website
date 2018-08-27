8c4b5d933af374a277128924e0dd0050015e1d99
Benjamin Eason <benjamin.eason@me.com>
2018-08-28 00:48:37 +0100

    more

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,33 @@
 <template>
 <div>
-  <b-navbar toggleable="md" type="light" class="p-0">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <b-navbar toggleable="md" type="light" class="p-0">
 
 
 
-    <b-navbar-brand>
-      <router-link to="/"><img alt="TB Johns logo" src="../assets/logo.png" width="180"></router-link>
-    </b-navbar-brand>
-    <b-navbar-toggle target="nav_collapse" class="mr-3"></b-navbar-toggle>
-    <b-collapse is-nav id="nav_collapse">
+          <b-navbar-brand>
+            <router-link to="/"><img alt="TB Johns logo" src="../assets/logo.png" width="180"></router-link>
+          </b-navbar-brand>
+          <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+          <b-collapse is-nav id="nav_collapse">
 
-      <b-navbar-nav>
-        <router-link class="nav-item" to="services">Services</router-link>
-        <router-link class="nav-item" to="contact">Contact</router-link>
-      </b-navbar-nav>
+            <b-navbar-nav>
+              <router-link class="nav-item" to="services">Services</router-link>
+              <router-link class="nav-item" to="contact">Contact</router-link>
+            </b-navbar-nav>
 
-      <b-navbar-nav class="ml-auto">
-          <span class="text-muted mr-3"><font-awesome-icon icon="phone" /><a class="font-weight-bold text-muted ml-2 no-link" href="tel://01724 763300">01724 763 300</a></span></b-navbar-brand>
-      </b-navbar-nav>
+            <b-navbar-nav class="ml-auto">
+                <span class="text-muted"><font-awesome-icon icon="phone" /><a class="font-weight-bold text-muted ml-2 no-link" href="tel://01724 763300">01724 763 300</a></span></b-navbar-brand>
+            </b-navbar-nav>
 
-    </b-collapse>
-  </b-navbar>
+          </b-collapse>
+        </b-navbar>
+      </div>
+    </div>
+  </div>
+
 
 
 </div>
