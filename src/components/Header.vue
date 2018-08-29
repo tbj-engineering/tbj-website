07e15a131e4a760ca3f0d360b82598d79a9632d7
Benjamin Eason <benjamin.eason@me.com>
2018-08-29 16:41:30 +0100

    More

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,7 @@
 
 
           <b-navbar-brand to="/" class="no-styling">
-          <img alt="TB Johns logo" src="../assets/logo.png" width="180">
+            <img alt="TB Johns logo" src="../assets/logo.png" width="180">
           </b-navbar-brand>
 
           <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
@@ -45,63 +45,64 @@ export default {
 
 
 <style lang="scss" scoped>
-  @import '../styles/variables.scss';
-  .nav-item {
-      margin: 0 5px;
-      text-decoration: none;
-      color: lighten($base,5%);
-      font-weight: 600;
+@import '../styles/variables.scss';
+.nav-item {
+    margin: 0 5px;
+    text-decoration: none;
+    color: lighten($base,5%);
+    font-weight: 600;
 
-      .nav-link {
-          border-bottom: 2px solid transparent;
-          color: $base;
-          &:hover {
-              color: $base;
-              border-color: #eaeaea;
-          }
-      }
-
-      .nav-link {
-          &.active {
-              border-color: $blue;
-          }
-      }
-
-  }
-
-  .no-styling {
-      .nav-link {
-          &.active {
-              border-color: transparent;
-          }
-
-      }
-  }
-
-  .no-link {
-      text-decoration: none;
-  }
-
-
-
-
-@media (max-width: 575.98px) {
-  #nav_collapse {
-
-
-      .nav-item {
-        text-align: center;
-        margin: 10px 0px;
-        .nav-link {
-          display: inline-block;
-
-        &.last-child {
-          margin-top: 10px;
-          margin-bottom: 20px;
+    .nav-link {
+        border-bottom: 2px solid transparent;
+        color: $base;
+        &:hover {
+            color: $base;
+            border-color: #eaeaea;
         }
-      }
+    }
+
+    .nav-link {
+        &.active {
+            border-color: $blue;
+        }
     }
 
 }
+
+.no-styling {
+    .nav-link {
+        &.active {
+            border-color: transparent;
+        }
+
+    }
+}
+
+.no-link {
+    text-decoration: none;
+}
+
+@media (max-width: 575.98px) {
+    #nav_collapse {
+        .nav-item {
+            text-align: center;
+            margin: 10px 0;
+            .nav-link {
+                display: inline-block;
+
+            }
+        }
+
+        .navbar-nav {
+        
+
+            &:last-child {
+                .nav-item {
+                    margin-top: 10px;
+                    margin-bottom: 20px;
+                }
+            }
+        }
+    }
 }
 </style>
