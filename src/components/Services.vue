ce536482910b64c2e85813ca3192935b995f1697
Benjamin Eason <benjamin.eason@me.com>
2018-08-30 10:07:41 +0100

    BUILD 290818-2

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -227,7 +227,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss">
+<style lang="scss" scoped>
 @import '../styles/variables.scss';
 
 .is-collapse > * {
