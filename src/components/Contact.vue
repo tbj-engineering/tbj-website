a69a0d63eaebc5d891c7669dd4f76e8056b835af
Benjamin Eason <benjamin.eason@me.com>
2018-09-04 13:38:52 +0100

    BUILD - 040918
    Updated links to telephone on contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -16,15 +16,15 @@
   <div class="row py-5">
     <div class="col-sm-6">
       <div class="pb-5">
-        <h4>Tom Johns <span class="small">Director</span></h4> M: 07721 318 105<br />
+        <h4>Tom Johns <span class="small">Director</span></h4> M: <a class="no-link" href="tel://07721 318 105">07721 318 105</a><br />
         E: tom@tbjohns.co.uk
       </div>
       <div class="pb-5">
-        <h4>Alex Johns <span class="small">Technical Director</span></h4> M: 07843 076 607<br />
+        <h4>Alex Johns <span class="small">Technical Director</span></h4> M: <a class="no-link" href="tel://07843 076 607">07843 076 607</a><br />
         E: alex@tbjohns.co.uk
       </div>
       <div class="pb-5">
-        <h4>Rebecca Johns <span class="small">Company Secretary</span></h4> M: 07540 385 430<br />
+        <h4>Rebecca Johns <span class="small">Company Secretary</span></h4> M: <a class="no-link" href="tel://07540 385 430">07540 385 430</a><br />
         E: beccajohns1977@hotmail.com
       </div>
 
